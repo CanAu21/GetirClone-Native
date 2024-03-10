f8f56825da817c733e785b59bfe4c15a8dd5c50d
CanAu21 <furkan.adrian9@outlook.com>
2024-03-09 23:03:10 -0800

    created banner component for the Home Page

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,44 +1,18 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
-import styles from "./styles";
-import { Entypo } from "@expo/vector-icons";
+import { ScrollView } from "react-native";
+import HeaderMain from "../../components/HeaderMain";
+import BannerCarousel from "../../components/BannerCarousel";
 
-const index = () => {
+function index() {
   return (
-    <View style={styles.headerMain}>
-      <View style={styles.headerOne}>
-        <Image
-          style={styles.image}
-          source={{ uri: "https://cdn.getir.com/misc/emoji/house.png" }}
-        />
-        <View style={styles.headerOneView}>
-          <Text style={{ fontWeight: 600, fontSize: 16 }}>Home</Text>
-          <Text
-            style={{
-              fontWeight: 500,
-              fontSize: 11.4,
-              color: "#6E7480",
-              marginLeft: 6,
-              marginRight: 1,
-            }}
-          >
-            Surry Hills Goulburn Street..
-          </Text>
-          <Entypo name="chevron-right" size={21} color="#5D3EBD" />
-        </View>
-        <View style={styles.headerTwo}>
-          <Text style={{ fontSize: 10, fontWeight: "bold", color: "#5D3EBD" }}>
-            TVS
-          </Text>
-          <Text style={{ fontSize: 20, fontWeight: "bold", color: "#5D3EBD" }}>
-            15<Text style={{ fontSize: 16, color: "#5D3EBD" }}>min</Text>
-          </Text>
-        </View>
-      </View>
-
-      <View></View>
-    </View>
+    <ScrollView
+      stickyHeaderIndices={[0]}
+      style={{ backgroundColor: "#f5f5f5" }}
+    >
+      <HeaderMain />
+      <BannerCarousel />
+    </ScrollView>
   );
-};
+}
 
 export default index;
